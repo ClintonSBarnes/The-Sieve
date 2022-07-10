f08965d55f9414c850c6a4b7dd712fa6fe51b1e6
ClintonSBarnes <clintbarnes8@gmail.com>
2022-07-09 21:43:44 -0500

    Add project files.

---
+++ The Sieve/Program.cs
@@ -0,0 +1,35 @@
+﻿using The_Sieve;
+int userNumberTested;
+int menuSelection;
+
+
+
+    Console.WriteLine($"How would you like to test this number?" +
+        $"\n1 - Check if EVEN number." +
+        $"\n2 - Check if MULTIPLE OF TEN number." +
+        $"\n3 - Check if POSITIVE number");
+    menuSelection = Convert.ToInt16(Console.ReadLine());
+
+    Sieve sieve = menuSelection switch
+    {
+        1 => new Sieve(IsEven),
+        2 => new Sieve(IsMultipleOfTen),
+        3 => new Sieve(IsPositive)
+    };
+
+while (true)
+{
+    Console.WriteLine("enter the number you would like to test.");
+    userNumberTested = Convert.ToInt16(Console.ReadLine());
+
+    Console.WriteLine(userNumberTested + "-" + sieve.IsGood(userNumberTested));
+}
+//
+
+bool IsEven(int number) => number % 2 == 0;
+bool IsMultipleOfTen(int number) => number % 10 == 0;
+bool IsPositive(int number) => number > 0;
+
+
+
+
---
+++ The Sieve/Sieve.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Sieve
+{
+    internal class Sieve
+    {
+        public  Func<int, bool> test;
+
+        public bool IsGood(int number)
+        {
+            return test(number);
+            
+        }
+
+        public Sieve(Func<int, bool> intakeFunction)
+        {
+         test = intakeFunction;
+
+        }
+
+    }
+}
